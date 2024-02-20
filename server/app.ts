1a8aa63a41e293a08e5494972f32afddbb489f4d
Pranav Aher <prranavyaher@gmail.com>
2024-02-20 12:40:53 +0530

    Setup project with typescript, mongodb, redis instance, cloudinary.

---
+++ server/app.ts
@@ -0,0 +1,38 @@
+require("dotenv").config();
+import express, {Request, Response, NextFunction} from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import {ErrorMiddleware} from "./middleware/error";
+import userRouter from "./routes/user.route";
+
+// body parser
+app.use(express.json({ limit: "50mb" }))
+
+// cookie parser
+app.use(cookieParser())
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+)
+
+app.use("/api/v1/", userRouter)
+
+// testing API
+app.get("/test",(req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working"
+  });
+})
+
+app.get("*", (req: Request, res: Response, next: NextFunction) => {
+  const err: any = new Error(`Route ${req.originalUrl} not found.`);
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(ErrorMiddleware)
