e739dd0fb9a0c64715a6250afdc44104bac79d86
Pranav Aher <prranavyaher@gmail.com>
2024-02-21 11:16:01 +0530

    Add route for updating refreshtoken.

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response, NextFunction} from "express";
-import { activateUser, loginUser, logoutUser, userRegistration } from "../controllers/user.controller"; 
+import { activateUser, loginUser, logoutUser, updateAccessToken, userRegistration } from "../controllers/user.controller"; 
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router()
 
@@ -7,5 +7,6 @@ userRouter.post('/registration', userRegistration)
 userRouter.post('/activate-user', activateUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/logout', isAuthenticated, logoutUser)
+userRouter.get('/refreshtoken', updateAccessToken)
 
 export default userRouter;
