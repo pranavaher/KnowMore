0e2b34a7f7a6774fd20bbbda9e70ee72cc6cf8f0
Pranav Aher <prranavyaher@gmail.com>
2024-02-22 12:28:00 +0530

    Add get api for single course.

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -66,3 +66,18 @@ export const editCourse = catchAsyncError(async(req: Request, res: Response, nex
     return next(new ErrorHandler(error.message, 500))
   }
 })
+
+// Get single course --- without purchasing
+export const getSingleCourse = catchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const course = await CourseModel.findById(req.params.id).select("-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links");
+    
+    res.status(201).json({
+      success: true,
+      course,
+    })
+
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 500))
+  }
+})
