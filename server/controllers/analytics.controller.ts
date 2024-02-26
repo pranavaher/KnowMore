9559a716a98f8e5bcfb4d35dcfa47d1cff84cad7
Pranav Aher <prranavyaher@gmail.com>
2024-02-26 14:41:02 +0530

    Develop function to generate Courses and Orders analytics.

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -18,6 +18,7 @@ import { idText } from "typescript";
 import notificationModel from "../models/notification.model";
 import { fetchAllCourses } from "../services/course.service"
 import { generateLast12MonthsData } from "../utils/analytics.generator";
+import orderModel from "../models/order.model";
 
 // Get Users analytics [ADMIN]
 export const getUsersAnalytics = catchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
@@ -27,6 +28,34 @@ export const getUsersAnalytics = catchAsyncError(async(req: Request, res: Respon
     res.status(201).json({
       success: true,
       users,
+    });
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+}) 
+
+// Get courses analytics [ADMIN]
+export const getCoursesAnalytics = catchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const courses = await generateLast12MonthsData(courseModel);
+    
+    res.status(201).json({
+      success: true,
+      courses,
+    });
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 500));
+  }
+}) 
+
+// Get order analytics [ADMIN]
+export const getOrderAnalytics = catchAsyncError(async(req: Request, res: Response, next: NextFunction) => {
+  try {
+    const order = await generateLast12MonthsData(orderModel);
+    
+    res.status(201).json({
+      success: true,
+      order,
     });
   } catch (error: any) {
     return next(new ErrorHandler(error.message, 500));
